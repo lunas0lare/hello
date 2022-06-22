41ee0c783c55da9a5407a185ea4272664b32ad01
lunas0lare <heartbreaker2605@gmail.com>
2022-06-23 01:55:18 +0700

    Update cc.cpp

--- test2/cc.cpp
+++ test2/cc.cpp
@@ -6,7 +6,10 @@ int main()
     int i = 0;
     for(int i = 0; i< 10;i++)
     {
-        cout <<"cc";
+        cout <<" hello";
     }
+    cout <<"create a branch 1";
+    cout<<"cc";
+    cout << "test undo commit";
     return 0;
-}
+}   
